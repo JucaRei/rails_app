5be424c3edd13a47ade3ed6e257817d7763446e3
JucaRei <reinaldo800@gmail.com>
2018-07-25 16:14:50 -0300

    salvando

--- app/models/artigos.rb
+++ app/models/artigos.rb
@@ -1,3 +1,6 @@
 class Artigo < ActiveRecord::Base
-
+  #Titulo nao pode ser nulo   #e tem um tamanho minimo e maximo
+  validates :titulo, presence:true, length: {minimum: 3, maximum: 50}
+  #descricao nao pode ser nula  #e tem um tamanho minimo e maximo
+  validates :descricao, presence: true, length: {minimum: 10, maximum: 300}
 end
